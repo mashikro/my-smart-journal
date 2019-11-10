ac1f8b10771c688733d17fdb5c3d8c8fd855a4e8
Masha Ikromova <mashikro@gmail.com>
2019-11-09 17:53:18 -0800

    Set up routes for displaying streak data

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ from model import connect_to_db, db, User, JournalEntry
 
 import bcrypt
 
-from streak import calculate_streak
+from streak import main
 
 from happiness import get_happiness_data
  
@@ -237,6 +237,43 @@ def show_hap_stats():
     #create logic, you cant see unless logged in
    
     return jsonify(data_dict)
+
+@app.route('/streak')
+def show_streak_chart():
+    '''Show streak data page'''
+    
+    user_id = session.get("user_id")
+    
+    main(user_id)
+    return '<html>lalala</html>'
+
+@app.route('/streak.json')
+def show_streak_stats():
+    '''Get data ready for streak page'''
+  
+    #user session to get which user
+    user_id = session.get("user_id")
+
+    # call func and pass in user_id as param
+    # result = calculate_streak(user_id)
+
+    # data_dict = {
+    #     "labels": result[0],
+    #     "datasets": [
+    #         {
+    #             "data": result[1],
+    #             "backgroundColor": [
+    #                 "#FF6384"
+    #             ],
+    #             "hoverBackgroundColor": [
+    #                 "#FF6384"
+    #             ],
+    #         }]
+    # }
+
+    # return jsonify(data_dict)
+    pass
+
 
 ####################### RUNNING MY SERVER ###############################
 if __name__ == "__main__":
