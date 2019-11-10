340a102969ef30a70e2f094953b82aff6941f85a
Masha Ikromova <mashikro@gmail.com>
2019-11-09 17:54:12 -0800

    Wrote a function to get user data and a function to generate date range

--- streak.py
+++ streak.py
@@ -1,73 +1,54 @@
 from model import User, JournalEntry, db
 
-from datetime import date, timedelta,datetime
+import datetime 
 
-def calculate_streak(user_email):
-    '''function that computes how many days in a row a user 
-    filled out their journal'''
+
+def get_entry_dates(user_id):
+    '''Query for user data'''
 
     print('=========== S T A R T ===================')
     
-    q = User.query.filter_by(email=user_email).first()
-    print('LOOOOOOOOOK', q)
+    q = User.query.get(user_id)
     
     list_entries = q.journal_entries
-    print(list_entries)
     
     list_of_dates = []
-    #list iterate over
+
     for entry in list_entries:
-        print(entry.date)
         list_of_dates.append(entry.date)
 
-    print(list_of_dates)
+    return list_of_dates
+
+def generate_date_range(start_date):
+    '''generates a range of date objects'''
+
+    current_date = datetime.date.today()
+    dates_range = [datetime.date.fromordinal(i) for i in range(start_date.toordinal(), 
+                    current_date.toordinal() +1)]
     
-    d = sorted(list_of_dates)
-    # print('SOOOOORTED', d)
-    # [datetime.date(2019, 11, 6), datetime.date(2019, 11, 7), datetime.date(2019, 11, 8), datetime.date(2019, 11, 10), datetime.date(2019, 11, 13), datetime.date(2019, 11, 14)]
-    
-    first_day = d[0]
-    current_date = date.today()
+    return dates_range
 
-    dates_dict = {}
+#input: list of datetime.date objects
+#return: 2 list in a tuple (datetime.date objects and ints)
+#first date - first entry
+#last date - today
+#no gaps
+def calculate_streak(all_dates):
+    '''function that computes how many days in a row a user 
+    filled out their journal'''
 
-    first_day_str = str(first_day)
-    current_date_str = str(current_date)
+    start_date = sorted(all_dates)[0]
+    dates_range = generate_date_range(start_date)
 
-    start = datetime.strptime(first_day_str, '%Y-%m-%d')
-
-    end = datetime.strptime(current_date_str, '%Y-%m-%d')
-
-    step = datetime.timedelta(days=1)
-
-    while start <= end:
-        print(start.date())
-        dates_dict[start.date()] = 0
-        start += step
-
-    print(dates_dict)
+    print(dates_range)
 
 
+def main(user_id):
+    all_dates = get_entry_dates(user_id)
+    result = calculate_streak(all_dates)
 
+    return result
 
-
-    # # Figure out which dates are missing
-    # date_set = set(d[0] + timedelta(x) for x in range((d[-1] - d[0]).days))
-    # # print('DATE SET', date_set)
-    # #{datetime.date(2019, 11, 6), datetime.date(2019, 11, 10), datetime.date(2019, 11, 9), datetime.date(2019, 11, 12), datetime.date(2019, 11, 7), datetime.date(2019, 11, 13), datetime.date(2019, 11, 11), datetime.date(2019, 11, 8)}
-    
-    # #list of missing dates
-    # missing = sorted(date_set - set(d))
-    # # MISSING DATE 2019-11-09
-    # # MISSING DATE 2019-11-11
-    # # MISSING DATE 2019-11-12
-
-    # for i in missing:
-    #     # print('MISSING DATE', i)
-
-
-    
-    print('=========== E N D ===================')
-
-
+#7th and 9th missing
+test_dates = [datetime.date(2019, 11, 6),  datetime.date(2019, 11, 8), datetime.date(2019, 11, 10), datetime.date(2019, 11, 13), datetime.date(2019, 11, 14)]
 
