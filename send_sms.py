4bfd80adeb718a61675494ed174b9551ef1af128
Masha Ikromova <mashikro@gmail.com>
2019-11-12 12:59:43 -0800

    Wrote function to run the scheduler: run_schedule(). Wrote a main function in charge of calling other functions

--- send_sms.py
+++ send_sms.py
@@ -54,30 +54,29 @@ def send_all_reminders():
 
 schedule.every().day.at("17:00").do(send_all_reminders) # UTC time 5pm is 9am PST
 
+
+def run_scheduler():
+    '''Run the schedule forever'''
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+def main():
+    '''Main func. Calls other functions'''
+    app = server.app
+    app.debug=True
+    model.connect_to_db(app)
+    print("connected to db.")
+    run_scheduler()
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
     
-#     schedule.every().day.at("9:30").do(job)
-
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
-
-
-
-
-
-# def job():
-#     print("I'm working...")
-
-
-# schedule.every(5).seconds.do(job)
-# schedule.every().hour.do(job)
-# schedule.every().monday.do(job)
-# schedule.every().wednesday.at("13:15").do(job)
-# schedule.every().minute.at(":17").do(job)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(1)
-
 #To verify people's num on my trial account: twilio.com/user/account/phone-numbers/verified
 
