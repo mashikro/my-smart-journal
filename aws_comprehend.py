e5fb59abcbaec58d298223cf70925034184a3ae1
Masha Ikromova <mashikro@gmail.com>
2019-11-15 15:33:48 -0800

    WIP: wrote functions: apply_sentiment_on_each_data() and main(). Able to make sentiment analysis for a list of journal entries

--- aws_comprehend.py
+++ aws_comprehend.py
@@ -5,7 +5,7 @@ import json
 
 client = boto3.client('comprehend')
 
-def get_query(user_id=1):
+def get_query(user_id):
     '''Use user_id to get user's journal entries for analysis'''
     q = model.User.query.get(user_id)
     list_entries = q.journal_entries
@@ -14,7 +14,9 @@ def get_query(user_id=1):
     list_of_inputs = []
 
     for entry in list_entries:
-        list_of_inputs.append(entry.q1_text)
+        list_of_inputs.append([entry.q1_text, 
+                                entry.q2_text,
+                                entry.q3_text])
 
     # print('LIST OF INPUTS:', list_of_inputs)
 
@@ -25,7 +27,7 @@ def aws_make_request(user_string):
     sentiment analysis and returns a response object'''
     
     response = client.detect_sentiment(
-        Text='masha is so stupid and ugly. jk!', #just text and not textlist
+        Text=user_string, #just text and not textlist
         LanguageCode='en'
     )
 
@@ -34,28 +36,29 @@ def aws_make_request(user_string):
     return response
 
 
-# def apply_sentiment_on_each_data(user_all_data):
-#     '''Applies AWS sentiment analysis on each piece of user data'''
-#     response_objects = []
+def apply_sentiment_on_each_data(user_all_data):
+    '''Applies AWS sentiment analysis on each piece of user data'''
+    response_objects = []
 
-#     for single_data in user_all_data:
-#         response = amazon_comprehend(single_data)
+    for single_data in user_all_data:
+        response = aws_make_request(single_data[0])
+        print('THIS IS EACH RESPONSE:', response)
 
-
-#     return response_objects
-
-
-# def organize_reponse_objects(responses):
-#     '''Takes in a list of response objects and returns (relevant data)'''
-
-#     pass
+    return response_objects
 
 
-# def main(user_id):
-#     user_all_entries = get_query(user_id)
-#     apply_sentiment_on_each_data(user_all_entries)
+def organize_reponse_objects(responses):
+    '''Takes in a list of response objects and returns (relevant data)'''
+
+    pass
 
 
+def main(user_id):
+    '''Main function uses helper functions to make API request'''
+    user_all_entries = get_query(user_id)
+    responses = apply_sentiment_on_each_data(user_all_entries)
+    print('==========================')
+    print('RESPONSES', responses)
 
 
 if __name__ == '__main__': 
