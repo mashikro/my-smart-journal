4940a8be4c521d127b094775a547c335553cfcd4
Masha Ikromova <mashikro@gmail.com>
2019-11-15 17:00:09 -0800

    set up for Sentiment Ananlysis visuals

---
+++ static/sentimentAnalysis.js
@@ -0,0 +1,53 @@
+"use strict";
+
+$(document).ready(() => {
+    // const options = {
+    //   responsive: true,
+    //   maintainAspectRatio: true,
+    //   legend: {
+    //         display: true,
+    //         label: {
+    //             fontColor: '#17cfcf',
+    //         }
+    //     },
+    //     title: {
+    //             display: true,
+    //             text: 'Your Journaling Habit Trend'
+    //     },
+    //   scales: {
+    //     yAxes: [{
+    //         ticks: {
+    //             suggestedMin: 0,
+    //         },
+    //         scaleLabel: {
+    //             display: true,
+    //             labelString: 'Streak Score'
+    //         }
+    //     }],
+    //     xAxes: [{
+    //         type: "time",
+    //         time: {
+    //             unit: 'day'
+    //         },
+    //         scaleLabel: {
+    //             display: true,
+    //             labelString: 'Date'
+    //         }
+    //     }]
+    //   }
+    // };
+
+    // let ctx = $('#streakChart').get(0).getContext('2d');
+
+    $.get("//sentiment-analysis.json", (data) => {
+
+        // let happyChart = new Chart(ctx, {
+        //                                 type: 'line',
+        //                                 data: data,
+        //                                 options: options
+        //                                 });
+
+        // $('#chartLegend').html(happyChart.generateLegend());
+        
+        });
+    });
