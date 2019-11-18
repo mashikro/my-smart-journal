8cdd182bc8e3eda0024969ce9b184e25b21708aa
Masha Ikromova <mashikro@gmail.com>
2019-11-18 13:00:12 -0800

    Refactored few functions to comply with new vision for sentiment analysis

--- aws_comprehend.py
+++ aws_comprehend.py
@@ -33,12 +33,13 @@ def aws_make_request(user_string):
     return response
 
 
-def apply_sentiment_on_each_data(user_all_data):
+def apply_sentiment_on_each_data(user_journal_entries):
     '''Applies AWS sentiment analysis on each piece of user data'''
     response_objects = []
 
-    for single_data in user_all_data:
-        response = aws_make_request(single_data[0])
+    for single_data in user_journal_entries:
+        print('SINGLE DATA', single_data)
+        response = aws_make_request(single_data)
         print('THIS IS EACH RESPONSE:', response)
         response_objects.append(response)
 
@@ -82,13 +83,28 @@ def organize_reponse_objects(responses):
 
 
 def main(user_id):
-    '''Main function uses helper functions to make API request'''
-   
-    user_all_entries = get_query(user_id)
-    responses = apply_sentiment_on_each_data(user_all_entries)
+    '''Main function uses helper functions to make API request
+    ((['POSITIVE', 'POSITIVE', 'NEUTRAL', 'NEUTRAL'], 
+    [(dict_keys(['Positive', 'Negative', 'Neutral', 'Mixed']), 
+    dict_values([0.5676356554031372, 0.0007096294430084527, 0.431611567735672, 4.310160147724673e-05])), 
+    (dict_keys(['Positive', 'Negative', 'Neutral', 'Mixed']), 
+    dict_values([0.7936040759086609, 0.004303410183638334, 0.20203734934329987, 5.515471275430173e-05])), 
+    (dict_keys(['Positive', 'Negative', 'Neutral', 'Mixed']), 
+    dict_values([0.3683869540691376, 0.006351249758154154, 0.625140905380249, 0.00012093513942090794])), 
+    (dict_keys(['Positive', 'Negative', 'Neutral', 'Mixed']), 
+    dict_values([0.00895390473306179, 0.027104999870061874, 0.9639381170272827, 3.0295936994662043e-06]))]), 
+    [datetime.date(2019, 11, 11), datetime.date(2019, 11, 13), datetime.date(2019, 11, 15), datetime.date(2019, 11, 15)])
+    '''
+    
+    # returns two item tuple (entries list and dates list)
+    journal_entries_dates = get_query(user_id)
+    #first item in 'journal_entries_dates' which is a list of all queries
+    responses = apply_sentiment_on_each_data(journal_entries_dates[0]) 
+    #returns two item tuple
     organized_reponses = organize_reponse_objects(responses)
 
-    return organized_reponses
+    #returning 'organized_response' which is a tuple w 2 items and list of dates
+    return (organized_reponses, journal_entries_dates[1]) 
 
 
 if __name__ == '__main__': 
