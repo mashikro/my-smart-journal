5cd0265310d32f2370ce449e6b8bf861edbbe5f2
Masha Ikromova <mashikro@gmail.com>
2019-11-19 08:21:22 -0800

    Adjusted the graph details for -bar- graph

--- static/sentimentAnalysis.js
+++ static/sentimentAnalysis.js
@@ -1,51 +1,51 @@
 "use strict";
 
 $(document).ready(() => {
-    // const options = {
-    //   responsive: true,
-    //   maintainAspectRatio: true,
-    //   legend: {
-    //         display: true,
-    //         label: {
-    //             fontColor: '#17cfcf',
-    //         }
-    //     },
-    //     title: {
-    //             display: true,
-    //             text: 'Your Journaling Habit Trend'
-    //     },
-    //   scales: {
-    //     yAxes: [{
-    //         ticks: {
-    //             suggestedMin: 0,
-    //         },
-    //         scaleLabel: {
-    //             display: true,
-    //             labelString: 'Streak Score'
-    //         }
-    //     }],
-    //     xAxes: [{
-    //         type: "time",
-    //         time: {
-    //             unit: 'day'
-    //         },
-    //         scaleLabel: {
-    //             display: true,
-    //             labelString: 'Date'
-    //         }
-    //     }]
-    //   }
-    // };
+    const options = {
+      responsive: true,
+      maintainAspectRatio: true,
+      legend: {
+            display: true,
+            label: {
+                fontColor: '#17cfcf',
+            }
+      },
+      title: {
+                display: true,
+                text: 'Sentiment Analysis for my Journal Entries'
+      },
+      scales: {
+        yAxes: [{
+            ticks: {
+                suggestedMin: 0,
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Sentiment Score'
+            }
+        }],
+        xAxes: [{
+            type: "time",
+            time: {
+                unit: 'day'
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Date'
+            }
+        }]
+      }
+    };
 
-    // let ctx = $('#streakChart').get(0).getContext('2d');
+    let ctx = $('#sentimentChart').get(0).getContext('2d');
 
-    $.get("//sentiment-analysis.json", (data) => {
-
-        // let happyChart = new Chart(ctx, {
-        //                                 type: 'line',
-        //                                 data: data,
-        //                                 options: options
-        //                                 });
+    $.get("/sentiment-analysis.json", (data) => {
+        console.log(data)
+        let sentimentChart = new Chart(ctx, {
+                                        type: 'bar',
+                                        data: data,
+                                        options: options
+                                        });
 
         // $('#chartLegend').html(happyChart.generateLegend());
         
