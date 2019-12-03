eb74c524106c439ebe75ccd2d076ed0f4ffa8145
Masha Ikromova <mashikro@gmail.com>
2019-12-03 12:18:08 -0800

    Removed some tests bc those pgs dont exist anymore

--- testing_flask.py
+++ testing_flask.py
@@ -2,6 +2,8 @@ import unittest
 
 from server import app
 
+import os
+
 from model import db, example_data, connect_to_db
 
 class TestFlaskRoutes(unittest.TestCase):
@@ -25,13 +27,6 @@ class TestFlaskRoutes(unittest.TestCase):
         self.assertIn(b"Login", result.data)
 
 
-    def test_create_account_page(self):
-        """Can we get to the create account page?"""
-
-        result = self.client.get("/create-account")
-        self.assertIn(b"Create Account", result.data)
-
-
     def test_home_page(self):
         """Can we reach the homepage?"""
 
@@ -39,57 +34,67 @@ class TestFlaskRoutes(unittest.TestCase):
         self.assertIn(b"On a scale of 1-5", result.data)
 
 
-    def test_login_page(self):
-        """Can we reach the login page?"""
+class TestDataBase(unittest.TestCase):
 
-        result = self.client.get("/login", follow_redirects=True)
-        self.assertIn(b"Welcome Back!", result.data)
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        # Get the Flask test client
+        self.client = app.test_client()
+
+        # Show Flask errors that happen during tests
+        app.config['TESTING'] = True
+
+        with self.client as c:
+            with c.session_transaction() as sess:
+                sess['user_id'] = 1
+       
+        # Connect to test database
+        connect_to_db(app, "testdb") #create testdb based on model.py
+
+        # # Create tables and add sample data
+        db.create_all()
+        example_data()
+
+
+    def tearDown(self):
+        """Stuff to do after each test."""
+        db.session.close()
+        db.drop_all()
+
+    def test_history_page(self):
+        """Can we reach the history page?"""
+
+        result = self.client.get("/history", follow_redirects=True)
+        self.assertIn(b"morning", result.data)
+
+
+    def test_single_entry_page(self):
+        """Can we reach the single entry page?"""
+
+        result = self.client.get("/view-entry/1", follow_redirects=True)
+        self.assertIn(b"happy, smiley", result.data)
+
 
     def test_profile_page(self):
+        """Can we reach the profile page?"""
 
         result = self.client.get("/profile", follow_redirects=True)
-        self.assertIn(b"Texting enabled", result.data)
+        self.assertIn(b"mbear@gmail.com", result.data)
 
 
-# class TestDataBase(unittest.TestCase):
+    def test_sentiment_page(self):
+        """Are we successfuly making an API request"""
 
-#     def setUp(self):
-#         """Stuff to do before every test."""
-
-#         # Get the Flask test client
-#         self.client = app.test_client()
-
-#         # Show Flask errors that happen during tests
-#         app.config['TESTING'] = True
-       
-#         # Connect to test database
-#         connect_to_db(app, "postgresql:///testdb") #create testdb based on model.py
-
-#         # # Create tables and add sample data
-#         db.create_all()
-#         example_data()
+        result = self.client.get("/sentiment-analysis")
+        self.assertEqual(result.status_code, 200)
 
 
-#     def tearDown(self):
-#         """Stuff to do after each test."""
-#         db.session.close()
-#         db.drop_all()
-#         pass
+    def  test_happy_page(self):
+        """Can we successfully reach the happiness page?"""
 
-
-#     def test_history_page(self):
-#         """Can we reach the history page?"""
-
-#         result = self.client.get("/history", follow_redirects=True)
-#         self.assertIn(b"History of Journal Entries", result.data)
-
-
-#     def test_single_entry_page(self):
-#         """Can we reach the history page?"""
-
-#         result = self.client.get("/view-entry/<int:entry_id>", follow_redirects=True)
-#         self.assertIn(b"Entry date", result.data)
-
+        result = self.client.get("/happy")
+        self.assertIn(b"Learn about what makes you happy", result.data)
 
 
 if __name__ == "__main__":
