9bafb73ea6b1c7376df329f16db35a2d07540f4e
Masha Ikromova <mashikro@gmail.com>
2019-12-08 11:38:37 -0800

    Updated words_to_ignore to NLTK english stopwords

--- aws_comp_key_phrases.py
+++ aws_comp_key_phrases.py
@@ -25,8 +25,19 @@ def count_data(q2_entries):
     for item in q2_entries:
         lst_of_words.extend(item.split())
 
-    words_to_ignore = {'the', 'a', 'is', 'with', 'for', 'an' 'and', 'or', 'of', 'to', 'on', 'my', 
-                        'they', 'i', 'more', 'many', 'while', 'lot', 'much', 'be', 'out', 'am'}
+    # words_to_ignore = {'the', 'a', 'is', 'with', 'for', 'an' 'and', 'or', 'of', 'to', 'on', 'my', 
+    #                     'they', 'i', 'more', 'many', 'while', 'lot', 'much', 'be', 'out', 'am'}
+    words_to_ignore = {"i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", 
+                        "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", 
+                        "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", 
+                        "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", 
+                        "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
+                        "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", 
+                        "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", 
+                        "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", 
+                        "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", 
+                        "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", 
+                        "very", "s", "t", "can", "will", "just", "don", "should", "now"}
     
     new_lst_words = []
     
