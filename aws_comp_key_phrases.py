3a57a0f2df7dd630d3af8f671253fe7928ab5238
Masha Ikromova <mashikro@gmail.com>
2020-03-27 12:42:40 -0400

    Commendted out unused code

--- aws_comp_key_phrases.py
+++ aws_comp_key_phrases.py
@@ -71,18 +71,18 @@ if __name__ == '__main__':
     print("connected to db.")
 
 
-########## Func for future feature (not being used yet) ######################
-import boto3
+# ########## Func for future feature (not being used yet) ######################
+# import boto3
 
-client = boto3.client('comprehend') 
+# client = boto3.client('comprehend') 
 
-def make_request(user_input):
-    '''Makes a request to AWS Comprehend API and returns a response object'''
+# def make_request(user_input):
+#     '''Makes a request to AWS Comprehend API and returns a response object'''
 
-    response = client.detect_key_phrases(
-        Text= user_input, #this needs to be a str
-        LanguageCode='en'  # |'es'|'fr'|'de'|'it'|'pt'|'ar'|'hi'|'ja'|'ko'|'zh'|'zh-TW'
-    )
+#     response = client.detect_key_phrases(
+#         Text= user_input, #this needs to be a str
+#         LanguageCode='en'  # |'es'|'fr'|'de'|'it'|'pt'|'ar'|'hi'|'ja'|'ko'|'zh'|'zh-TW'
+#     )
 
 
 
